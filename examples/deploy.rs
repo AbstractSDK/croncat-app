a4b3b0be1b19ce49e6ba92675bd47dc05b843814
Buckram <buckram123@gmail.com>
2023-06-27 14:27:15 +0300

    Install app script

--- examples/deploy.rs
+++ examples/deploy.rs
@@ -1,11 +1,16 @@
+use abstract_core::{app::BaseInstantiateMsg, objects::gov_type::GovernanceDetails};
 use cw_orch::{
     anyhow,
-    prelude::{networks::parse_network, DaemonBuilder},
+    deploy::Deploy,
+    prelude::{networks::parse_network, ContractInstance, DaemonBuilder, TxHandler},
     tokio::runtime::Runtime,
 };
 
-use abstract_interface::AppDeployer;
-use app::contract::{interface::CroncatApp, CRONCAT_ID};
+use abstract_interface::*;
+use app::{
+    contract::{interface::CroncatApp, CRONCAT_ID},
+    msg::AppInstantiateMsg,
+};
 use dotenv::dotenv;
 use semver::Version;
 
@@ -21,8 +26,43 @@ fn main() -> anyhow::Result<()> {
         .chain(chain)
         .handle(rt.handle())
         .build()?;
-    let app = CroncatApp::new(CRONCAT_ID, chain);
+    let app = CroncatApp::new(CRONCAT_ID, chain.clone());
 
-    app.deploy(version)?;
+    // It's already deployed
+    // app.deploy(version)?;
+
+    // Create account
+    let abstract_deployment = Abstract::load_from(chain.clone())?;
+    let account = abstract_deployment.account_factory.create_default_account(
+        GovernanceDetails::Monarchy {
+            monarch: chain.sender().into_string(),
+        },
+    )?;
+
+    // In case account already created
+
+    // let account_base = abstract_deployment.version_control.get_account(7)?;
+    // let account = AbstractAccount::new(chain.clone(), None);
+    // account.manager.set_address(&account_base.manager);
+    // account.proxy.set_address(&account_base.proxy);
+
+    // Claim namespace
+    let account_config = account.manager.config()?;
+    abstract_deployment.version_control.claim_namespaces(
+        account_config.account_id.u64() as u32,
+        vec!["croncat".to_owned()],
+    )?;
+
+    // Install app
+    account.install_module(
+        CRONCAT_ID,
+        &app::msg::InstantiateMsg {
+            base: BaseInstantiateMsg {
+                ans_host_address: abstract_deployment.ans_host.addr_str()?,
+            },
+            module: AppInstantiateMsg {},
+        },
+        None,
+    )?;
     Ok(())
 }
